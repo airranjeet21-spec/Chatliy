012274960f5c8f485a9028c5f97b79cfe8fec6df
Durga Ram <airranjeet21@gmail.com>
2026-02-06 19:47:03 +0530

    Update message.model.js

--- Backend/module/message.model.js
+++ Backend/module/message.model.js
@@ -17,7 +17,6 @@ const messageSchema = new mongoose.Schema(
     image: {
       type: String, default: ""
     },
-    // --- YE ADD KARO ---
     replyTo: {
       text: { type: String, default: "" },
       img: { type: String, default: "" },
