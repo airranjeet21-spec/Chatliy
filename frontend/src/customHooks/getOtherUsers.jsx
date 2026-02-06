38a3c3a25fd8ce564bef2df956965e3c38579e51
Durga Ram <airranjeet21@gmail.com>
2026-02-06 17:34:25 +0530

    Update getOtherUsers.jsx

--- frontend/src/customHooks/getOtherUsers.jsx
+++ frontend/src/customHooks/getOtherUsers.jsx
@@ -1,24 +1,49 @@
 
+// import { useEffect } from "react";
+// import axios from "axios";
+// import { serverUrl } from "../config.js";
+// import { useDispatch, useSelector } from "react-redux";
+// import { setotherUsers } from "../redux/userSlice";
+// const getOtherUsers = () => {
+//   let dispatch = useDispatch();
+// let {userData}=useSelector(state=>state.user)
+//   useEffect(() => {
+//   if (!userData) return; 
+//   const fetchUser = async () => {
+//     try {
+//       let result = await axios.get(`${serverUrl}/api/user/others`, { withCredentials: true });
+//       dispatch(setotherUsers(result.data));
+//     } catch (error) {
+//       console.log("Other User error:", error);
+//     }
+//   };
+//   fetchUser();
+// }, [dispatch, userData?._id]);  
+// };
+
+// export default getOtherUsers;
 import { useEffect } from "react";
 import axios from "axios";
 import { serverUrl } from "../config.js";
 import { useDispatch, useSelector } from "react-redux";
 import { setotherUsers } from "../redux/userSlice";
-const getOtherUsers = () => {
-  let dispatch = useDispatch();
-let {userData}=useSelector(state=>state.user)
+
+const useOtherUsers = () => {
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+
   useEffect(() => {
-  if (!userData) return; 
-  const fetchUser = async () => {
-    try {
-      let result = await axios.get(`${serverUrl}/api/user/others`, { withCredentials: true });
-      dispatch(setotherUsers(result.data));
-    } catch (error) {
-      console.log("Other User error:", error);
-    }
-  };
-  fetchUser();
-}, [dispatch, userData?._id]);  
+    if (!userData) return; 
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get(`${serverUrl}/api/user/others`, { withCredentials: true });
+        dispatch(setotherUsers(result.data));
+      } catch (error) {
+        console.log("Other User error:", error);
+      }
+    };
+    fetchUser();
+  }, [dispatch, userData?._id]); 
 };
 
-export default getOtherUsers;
+export default useOtherUsers;
