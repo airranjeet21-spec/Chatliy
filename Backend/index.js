f55f2370d01a7cbb8d5994da01359a5c38c8f81f
Durga Ram <airranjeet21@gmail.com>
2026-02-06 13:52:39 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ import { app, server } from "./socket/socket.js";
 
 const port = process.env.PORT || 8000;
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chatliy.onrender.com",
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" })); 
