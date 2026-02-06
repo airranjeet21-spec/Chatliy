467f01b5bd48e7589695d517aff277b2025171b0
Durga Ram <airranjeet21@gmail.com>
2026-02-06 13:53:19 +0530

    Update socket.js

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chatliy.onrender.com",
     methods: ["GET", "POST"],
     credentials: true,
   }
