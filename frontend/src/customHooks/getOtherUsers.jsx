86144879439ffa704c5fd3f4331fa8f725b16658
Durga Ram <airranjeet21@gmail.com>
2026-02-06 19:14:34 +0530

    Update getOtherUsers.jsx

--- frontend/src/customHooks/getOtherUsers.jsx
+++ frontend/src/customHooks/getOtherUsers.jsx
@@ -1,27 +1,3 @@
-
-// import { useEffect } from "react";
-// import axios from "axios";
-// import { serverUrl } from "../config.js";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setotherUsers } from "../redux/userSlice";
-// const getOtherUsers = () => {
-//   let dispatch = useDispatch();
-// let {userData}=useSelector(state=>state.user)
-//   useEffect(() => {
-//   if (!userData) return; 
-//   const fetchUser = async () => {
-//     try {
-//       let result = await axios.get(`${serverUrl}/api/user/others`, { withCredentials: true });
-//       dispatch(setotherUsers(result.data));
-//     } catch (error) {
-//       console.log("Other User error:", error);
-//     }
-//   };
-//   fetchUser();
-// }, [dispatch, userData?._id]);  
-// };
-
-// export default getOtherUsers;
 import { useEffect } from "react";
 import axios from "axios";
 import { serverUrl } from "../config.js";
