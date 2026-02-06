f590b59dec47787cf5933da45907978d86314122
Durga Ram <airranjeet21@gmail.com>
2026-02-06 18:14:40 +0530

    Update message.controllers.js

--- Backend/controllers/message.controllers.js
+++ Backend/controllers/message.controllers.js
@@ -9,16 +9,11 @@ export const SendMessage = async (req, res) => {
     // req.body se replyTo bhi nikaalo
     const { message, replyTo } = req.body; 
 
-    // let image;
-    // if (req.file) {
-    //   image = await uploadOnCloudinary(req.file.path);
-    // }
-    // editProfile controller mein:
-if (req.file) {
-    // req.file.path ki jagah req.file.buffer pass karein
-    const imageUrl = await uploadOnCloudinary(req.file.buffer); 
-    updateData.image = imageUrl;
-}
+    let image;
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+    }
+
 
     // JSON parse karna padega kyunki frontend se FormData ke through Stringify karke aata hai
     let parsedReply = null;
