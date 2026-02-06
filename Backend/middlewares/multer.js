7fb350b26ff514bd84a5b20d2fc56f4925b43477
Durga Ram <airranjeet21@gmail.com>
2026-02-06 19:46:00 +0530

    Update multer.js

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -1,24 +1,9 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "./public/temp"); 
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, Date.now() + "-" + file.originalname);
-//     }
-// });
-
-// const upload = multer({ storage });
-// export default upload;
 import multer from "multer";
-
-// memoryStorage use karne se "public/temp" ka jhanjhat khatam
 const storage = multer.memoryStorage();
 
 const upload = multer({ 
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit tak ki image
+    limits: { fileSize: 5 * 1024 * 1024 } 
 });
 
 export default upload;
