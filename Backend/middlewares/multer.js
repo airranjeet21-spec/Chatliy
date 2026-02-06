c33ee9cc1fb6642332d274d044799179042ea9a7
Durga Ram <airranjeet21@gmail.com>
2026-02-06 17:54:17 +0530

    Update multer.js

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -1,13 +1,24 @@
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./public/temp"); 
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     }
+// });
+
+// const upload = multer({ storage });
+// export default upload;
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp"); 
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
+
 export default upload;
