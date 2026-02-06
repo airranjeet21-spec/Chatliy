d01eb22fa096e55434a05bf0c5d97de6315f17d2
Durga Ram <airranjeet21@gmail.com>
2026-02-06 19:26:37 +0530

    Update cloudinary.js

--- Backend/Config/cloudinary.js
+++ Backend/Config/cloudinary.js
@@ -1,22 +1,3 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import fs from"fs"
-// const uploadOnCloudinary=async(filepath)=>{
-// cloudinary.config({ 
-//         cloud_name:process.env.CLOUD_NAME, 
-//         api_key:process.env.API_KEY, 
-//         api_secret:process.env.API_SECRET 
-//     });
-//     try {
-//      const uploadResult = await cloudinary.uploader
-//        .upload(filepath);
-//        fs.unlinkSync(filepath);
-//        return uploadResult.secure_url  
-//     } catch (error) {
-//       fs.unlinkSync(filepath)
-//       console.log(error)
-//     }
-// }
-// export default uploadOnCloudinary;
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({ 
